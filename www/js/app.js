267823e388dd1a269310d9c5a1520e1c7c48e99a
Thiago Tibau <ttibaudev@gmail.com>
2016-08-23 21:00:57 -0300

    First Commit
    bolada

---
+++ www/js/app.js
@@ -0,0 +1,116 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.factory('ListaSelecionado', function(){
+  dados = {};
+  dados.nome = '';
+  dados.endereco = '';
+  dados.telefone = '';
+  return dados;
+})
+
+.factory('ItemSelecionado', function(){
+  item = {};
+  item.nome = '';
+  item.endereco = '';
+  item.telefone = '';
+  return item;
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.vereador',{
+    url: '/vereador',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/vereador.html',
+        controller: 'PlaylistsCtrl'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html',
+          controller: 'listaCtrl'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+    .state('selecionado', {
+      url: '/selecionado',
+      templateUrl: 'templates/selecionado.html',
+      controller: 'itemSelecionadoCtrl'
+    })
+
+    .state('app.lista', {
+      url: '/lista',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/lista.html',
+          controller: 'listaCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,93 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $location, $ionicPopup) {
+
+
+  $scope.refresh = function(){
+    $ionicPopup.alert({
+      title: 'Estamos quase lá!',
+      template: 'Aguarde até o dia das apurações ;D'
+    });
+    $scope.$broadcast('scroll.refreshComplete');
+  };
+
+  $scope.mostraMensagem = function(){
+    var alerta = $ionicPopup.alert({
+      title: 'Aviso',
+      template: 'Atenção, a apuração estará disponível no dia 1/10/2016 <strong>primeiro turno</strong>, o resultado será feito em tempo real'
+    });
+  };
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  $scope.voltar = function(){
+    $scope.closeLogin();  
+  };
+
+})
+
+.controller('VereadorCtrl', function($scope){
+
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+
+})
+
+.controller('PlaylistCtrl', function($scope) {
+  
+})
+
+.controller('listaCtrl', function($scope, $firebaseObject, $ionicPopup, $location, ListaSelecionado){
+  var ref = new Firebase("https://riobonito-92bac.firebaseio.com/");
+
+$scope.pegaDados = function (data){
+  $scope.dados = $firebaseObject(ref.child(data));
+  $location.path('/app/lista');
+  ListaSelecionado.dados = $scope.dados;
+  console.log(ListaSelecionado.dados);
+  };
+
+
+})
+
+.controller('listaItemCtrl', function($scope, $firebaseObject, $ionicPopup, ListaSelecionado, $ionicModal, ItemSelecionado){
+  //console.log(ListaSelecionado.dados);
+  $scope.dados = ListaSelecionado.dados;
+  // Crio a intancia e injeto no scopo $scope.modal
+   $ionicModal.fromTemplateUrl('templates/selecionado.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+   }).then(function(modal){
+    $scope.modal = modal;
+   });
+   //abre a modal
+   $scope.abrirModal = function(dados){
+      $scope.modal.show();
+      ItemSelecionado.item = dados; 
+      $scope.item = ItemSelecionado.item;
+      console.log(ItemSelecionado.item);
+   };
+
+   //fecha a modal
+   $scope.fecharModal = function(){
+      $scope.modal.hide();
+   };
+   
+})
+
